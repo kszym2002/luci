3440fa61b08ba9e7b413fd64c4ade73a9f238e2d
kszym2002 <kszym2002@gmail.com>
2016-11-22 10:31:53 +0800

    Update proto_openconnect.lua

--- protocols/luci-proto-openconnect/luasrc/model/network/proto_openconnect.lua
+++ protocols/luci-proto-openconnect/luasrc/model/network/proto_openconnect.lua
@@ -9,37 +9,4 @@ function proto.get_i18n(self)
 	return luci.i18n.translate("OpenConnect (CISCO AnyConnect)")
 end
 
-function proto.ifname(self)
-	return "vpn-" .. self.sid
-end
 
-function proto.get_interface(self)
-	return interface(self:ifname(), self)
-end
-
-function proto.opkg_package(self)
-	return "openconnect"
-end
-
-function proto.is_installed(self)
-	return nixio.fs.access("/lib/netifd/proto/openconnect.sh")
-end
-
-function proto.is_floating(self)
-	return true
-end
-
-function proto.is_virtual(self)
-	return true
-end
-
-function proto.get_interfaces(self)
-	return nil
-end
-
-function proto.contains_interface(self, ifc)
-	 return (netmod:ifnameof(ifc) == self:ifname())
-
-end
-
-netmod:register_pattern_virtual("^vpn-%w")
